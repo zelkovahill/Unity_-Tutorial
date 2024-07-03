dbf41c533c2b6171d99bb1956ab6c1abd1bdac3d
KAWAI~ WOOMINCHAN~ <changwoomin426@gmail.com>
2024-07-03 22:35:34 +0900

    feat : 파워업 및 카운트다운

--- Assets/Scripts/Prototype 4/PlayerController4.cs
+++ Assets/Scripts/Prototype 4/PlayerController4.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class PlayerController4 : MonoBehaviour
 {
     public float speed;
+    private float powerupStrength = 15.0f;
+    
+    public GameObject powerupIndicator;
+    
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerUp;
 
     private void Start()
     {
@@ -17,7 +22,39 @@ public class PlayerController4 : MonoBehaviour
 
     private void Update()
     {
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            powerupIndicator.gameObject.SetActive(true);
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position;
+            
+            Debug.Log("Collided with " + other.gameObject.name+ " with powerup set to " + hasPowerUp);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+        }
     }
 }
