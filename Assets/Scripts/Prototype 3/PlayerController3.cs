f8eaae4658975d0fdbadf715332df2688caf7bb8
KAWAI~ WOOMINCHAN~ <changwoomin426@gmail.com>
2024-07-03 17:43:30 +0900

    feat : 파티클 및 음향효과 적용

--- Assets/Scripts/Prototype 3/PlayerController3.cs
+++ Assets/Scripts/Prototype 3/PlayerController3.cs
@@ -13,21 +13,35 @@ public class PlayerController3 : MonoBehaviour
     public bool gameOver = false;
 
     private Animator playerAnim;
+    
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    
+   
+    
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            playerAudio.PlayOneShot(jumpSound, 1f);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
@@ -37,13 +51,17 @@ public class PlayerController3 : MonoBehaviour
        if(other.gameObject.CompareTag("Ground"))
        {
            isOnGround = true;
+           dirtParticle.Play();
        }
        else if(other.gameObject.CompareTag("Obstacle"))
        {
+           explosionParticle.Play();
            gameOver = true;
            Debug.Log("Game Over!");
            playerAnim.SetBool("Death_b", true);
            playerAnim.SetInteger("DeathType_int", 1);
+           dirtParticle.Stop();
+           playerAudio.PlayOneShot(crashSound, 0.5f);
        }
     }
 }
