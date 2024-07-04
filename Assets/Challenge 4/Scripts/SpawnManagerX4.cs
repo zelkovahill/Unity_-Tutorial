f30532a28449f39089a44a091948b076605c9c77
KAWAI~ WOOMINCHAN~ <changwoomin426@gmail.com>
2024-07-04 14:10:27 +0900

    feat : 축구 스크립팅

---
+++ Assets/Challenge 4/Scripts/SpawnManagerX4.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpawnManagerX4 : MonoBehaviour
+{
+   
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+
+
+    public GameObject player;
+
+    private void Start()
+    {
+        enemyPrefab.GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length; // 내가 별로 좋아하지 않는 코드 비용이 많이 든다.
+        // 강의에서는 FindObjectsOfType<Enemy>().Length; 를 사용했기 때문에 이것을 사용해야 한다.
+        
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(waveCount);
+        }
+
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+        
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+        
+        
+        waveCount++;
+        
+        ResetPlayerPosition(); // put player back at start
+
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+
+}
